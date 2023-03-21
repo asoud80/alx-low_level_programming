97e13f5099fdb3f9e59c22a2c008513f82d11a36
asoud80 <amgad_a.soud@outlook.com>
2023-03-21 13:18:01 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,11 +8,12 @@
 
 void print_alphabet(void)
 {
+
 		char letter;
 
 		for (letter = 'a'; letter <= 'z'; letter++)
-			{
-			_putchar(letter);
-			}
+		{
+		_putchar(letter);
+		}
 		_putchar('\n');
 }
