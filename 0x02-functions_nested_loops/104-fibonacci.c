426e22a75050f13cb2d4e35c317ca3a834bdf5b8
asoud80 <amgad_a.soud@outlook.com>
2023-03-21 14:33:16 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
+/**
+ * main - main block
+ * Description: computes and prints the sum of all the multiples of 3 or
+ * 5 below 1024 (excluded), followed by a new line
+ * Return: 0
+ */
+int main(void)
 
-
-	/**
-	 * main - fibonacci <3
-	 *
-	 * Purpose - no hardcode
-	 *
-	 * Return:  (Success)
-	 */
-
-
-	int main(void)
 	{
 		unsigned long int i;
 		unsigned long int bef = 1;
