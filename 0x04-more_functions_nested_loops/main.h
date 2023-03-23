378f0e3d9d70caf6d22803f8d0afe54d86c5985d
asoud80 <amgad_a.soud@outlook.com>
2023-03-23 13:13:41 -0700

    0-isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-
 int _putchar(char c);
 int _isupper(int c);
 void print_alphabet(void);
@@ -17,5 +12,3 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
-#endif
