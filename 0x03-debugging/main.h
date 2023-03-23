c299b045a30a4d9bdaacceca9691ab467e080ca0
asoud80 <amgad_a.soud@outlook.com>
2023-03-23 04:08:07 -0700

    This program prints the largest of three integers

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,6 +5,7 @@ carrie@ubuntu:/debugging$ cat main.h
 #include <stdio.h>
 
 void positive_or_negative(int i);
+largest = largest_number(a, b, c);
 
 #endif /* MAIN_H */
-carrie@ubuntu:/debugging$ 
+carrie@ubuntu:/debugging$i 
