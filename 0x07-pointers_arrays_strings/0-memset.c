2f9f87654ec9382e385fbf942398568beaa3e16a
asoud80 <amgad_a.soud@outlook.com>
2023-04-03 14:18:08 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _memset - ironnou
@@ -14,8 +13,8 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-		{
+	{
 		s[i] = b;
-		}
+	}
 	return (s);
 }
