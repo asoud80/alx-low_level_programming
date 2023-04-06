3b893a1a2041a6a654a85a240ee9ae069663c12a
asoud80 <amgad_a.soud@outlook.com>
2023-04-05 18:00:42 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _puts_recursion - Prints a string followed by a new line
  * @s: string
@@ -8,14 +9,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s == 0)
 	{
 		_putchar('\n');
+		return;
 	}
-	else
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	return (0);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
