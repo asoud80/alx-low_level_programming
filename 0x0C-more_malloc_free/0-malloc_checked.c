d0e825d0554028683e000bc93bf34a94ed35a818
asoud80 <amgad_a.soud@outlook.com>
2023-04-13 02:48:49 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,17 +1,13 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
-*malloc_checked - function that allocates memory using malloc.
-*
-*@b: This is the length to allocate in the memory
-*
-*Return: Returns a pointer to the allocated memory
-*if malloc fails, the malloc_checked function should cause normal 
-*process
-*termination with a status value of 98
-*
-*/
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
+ */
+
 void *malloc_checked(unsigned int b)
 {
 	char *i;
