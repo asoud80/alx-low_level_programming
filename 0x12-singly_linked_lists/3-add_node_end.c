21b8206717f2123331e27e494dfa06a165b45dc2
asoud80 <amgad_a.soud@outlook.com>
2023-04-27 18:14:20 -0700

    2-add_node.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * len - calculates the length of string
+ * @str: constant string 
+ * @Return: the length of string
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+
+
