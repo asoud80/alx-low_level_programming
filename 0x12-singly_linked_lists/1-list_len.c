b85d47108bd63485f8337ec0582da6f2399fda54
asoud80 <amgad_a.soud@outlook.com>
2023-04-27 17:33:10 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,13 +10,16 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t elements = 0;
+	size_t list_len(const list_t *h)
+{
+	int count = 0;
 
-	while (h)
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
 	{
-		elements++;
+		count += 1;
 		h = h->next;
 	}
-
-	return (elements);
+	return (count);
 }
