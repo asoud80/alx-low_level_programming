a750862bdbcccdd5a5233cddcb64dcc6280436e9
asoud80 <amgad_a.soud@outlook.com>
2023-04-27 19:04:34 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -26,9 +26,8 @@ list_t *add_node(list_t **head, const char *str)
 		free(new);
 		return (NULL);
 	}
-
-	for (len = 0; str[len]; len++);
-
+	for (len = 0; str[len];)
+		len++;
 	new->str = dup;
 	new->len = len;
 	new->next = *head;
