6e40cdf0390830e6ad09e6711b676d0d0dd5e856
asoud80 <amgad_a.soud@outlook.com>
2023-04-28 19:42:12 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,3 +1,23 @@
+/**
+ * free_list - frees a list_t list
+ * @head: A pointer to the head node of the list
+ *
+ * Description: the function frees each node of a list_t linked list
+ *
+ * Return nothing
+ */
+
 #include "lists.h"
 
+void free_list(list_t *head)
+{
+	list_t *current;
 
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
