2879845b96fe38ca412286b6fe5d49d10238b221
asoud80 <amgad_a.soud@outlook.com>
2023-05-01 00:02:01 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,17 +10,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
-	if (head == NULL)
+	while (head != NULL)
 	{
-		return (NULL);
-	}
-
-	for (i = 0; i < index; i++)
-	{
+		if (i == index)
+			return (head);
+		i++;
 		head = head->next;
-		if (head == NULL)
-			return (NULL);
 	}
-	return (head);
+	return (NULL);
 }
 
