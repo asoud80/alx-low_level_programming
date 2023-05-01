5227134370c3bf0abf135cf684b772b4071d7c78
asoud80 <amgad_a.soud@outlook.com>
2023-04-30 20:46:09 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * free_listint2 - frees a list
+ * @head: address of the first node of a list.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t tmp;
+
+	if (head == NULL)
+		return;
+
+		while (*head != NULL)
+		{
+			tmp = (*head)->next;
+			free(head);
+			*head = tmp;
+		}
+}
