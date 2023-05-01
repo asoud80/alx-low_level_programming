d926950b28f17178642297f3cc2b35ffb4f437f4
asoud80 <amgad_a.soud@outlook.com>
2023-05-01 00:07:01 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - function to find nth of node
+ * get_nodeint_at_index - Function returns to nth of node
  * @head: address of the first node in a list
  * @index: position of the node to find (starting from 0)
  * @Return: this is the return of node address.
@@ -16,7 +16,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 			return (head);
 		i++;
 		head = head->next;
+		return (NULL);
 	}
-	return (NULL);
+	return (head);
 }
 
