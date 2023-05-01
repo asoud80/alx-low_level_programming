1078c96ba2ed784c099ed9bd40f3d4535e28bcdd
asoud80 <amgad_a.soud@outlook.com>
2023-04-30 21:19:25 -0700

    6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletesand returns the head node's data
+ * @head: this my structure
+ * Return: Return the head node's data(n)
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	itn data;
+
+	if (*head == NULL)
+		return (0);
+	tmp = *head;
+	*head = (*head)->next;
+	data = tmp->n;
+	free(tmp);
+	return (data);
+}
