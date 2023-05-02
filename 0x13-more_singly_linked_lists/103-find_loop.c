ce961f3c7d70d69d4b6228dfd78372ae27babe5e
asoud80 <amgad_a.soud@outlook.com>
2023-05-02 16:54:43 -0700

    u

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -6,11 +6,10 @@
  *
  * Return: the address of the node where the loop starts.
  */
-listint_t *find_listint_loop(listint-t *head)
+listint_t *find_listint_loop(listint_t *head)
 {
-{
-	listint-t *p2;
-	listint-t *prev;
+	listint_t *p2;
+	listint_t *prev;
 
 	p2 = head;
 	prev = head;
