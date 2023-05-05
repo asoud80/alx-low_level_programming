0c93aa666ee20a6e6a3999705f58cd7191c97e93
asoud80 <amgad_a.soud@outlook.com>
2023-05-04 19:18:02 -0700

    4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * clear_bit - clears the bit at index
+ * @n: the number to index
+ * @index: the bit to clear
+ *
+ * Return: 1 for success, -1 for error.
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+
+	if (*n & 1L << index)
+		*n ^= 1L << index;
+	return (1);
+}
