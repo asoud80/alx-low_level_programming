109301c46a5a5ddd88917e021a3157203caba747
asoud80 <amgad_a.soud@outlook.com>
2023-05-09 14:14:49 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,17 +10,17 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fp;
+	int fd;
 	ssize_t bytes;
 	char buf[READ_BUF_SIZE * 8];
 
 	if (!filename || letters)
 		return (0);
-	fp = open(filename, 0_RDONLY);
-	if (fp == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-	bytes = read(fp, &buf[0], letters);
+	bytes = read(fd, &buf[0], letters);
 	bytes = write(STDOUT_FILENO, &buf[0], bytes);
-	close(fp);
+	close(fd);
 	return (bytes);
 }
