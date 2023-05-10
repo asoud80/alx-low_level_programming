1afec27e6eab7932d25df94b92e0a028153947f4
asoud80 <amgad_a.soud@outlook.com>
2023-05-10 11:10:44 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,7 +32,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_RDONLY | O_CREAT | O_TRUNC, O_RDWR, 0600);
+	fd = open(filename, O_RDONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (len)
