556ffedead9128942c96b4838c96fd55dbd994b5
asoud80 <amgad_a.soud@outlook.com>
2023-05-10 11:50:51 -0700

    1-create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fp, length = 0, fpWrite;
+	int fp, length = 0, fpwrite;
 
 	fp = open(filename, O_CREAT | O_TRUNC | O_RWONLY, 0600);
 
@@ -28,9 +28,9 @@ int create_file(const char *filename, char *text_content)
 		length++;
 	}
 
-	fpWrite = Write(fp, text_content, length);
+	fpwrite = write(fp, text_content, length);
 
-	if (fpWrite == -1)
+	if (fpwrite == -1)
 	{
 		return (-1);
 	}
