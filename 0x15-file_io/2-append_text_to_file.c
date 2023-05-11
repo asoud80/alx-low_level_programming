0d7811a03f7634e21d20ba02bfe75353bedd9204
asoud80 <amgad_a.soud@outlook.com>
2023-05-11 07:22:59 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- * append_text_to_file - Appends text at the end of a file
+ * append_text_to_file: append_text_to_file - append description
  *
- * @filename: Const char pointer
+ * @filename : Const char ptr
  *
- * @text_content: Char pointer
+ * @text_content : Char ptr
  *
  * Return: int
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fdopen, fdWrite, length = 0;
+	int fbopen, bdWrite, length = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fdopen = open(filename, O_WRONLY | O_APPEND);
+	fbopen = open(filename, O_WRONLY | O_APPEND);
 
-	if (fdopen == -1)
+	if (fbopen == -1)
 	{
 		return (-1);
 	}
@@ -29,11 +29,11 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		length++;
 	}
-	fdWrite = write(fdopen, text_content, length);
+	fbWrite = write(fbopen, text_content, length);
 
-	if (fdWrite == -1)
+	if (fbWrite == -1)
 		return (-1);
-	close(fdopen);
+	close(fbopen);
 
 	return (1);
 }
