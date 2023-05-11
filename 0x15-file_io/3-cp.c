993475750e2c2b447cc04dcd6f4700223d9a7ff4
asoud80 <amgad_a.soud@outlook.com>
2023-05-11 06:17:50 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
 	char buffer[1024];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
@@ -25,30 +25,30 @@ int main(int argc, char **argv)
 	}
 	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
-		dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	while ((filecheck = read(fd, buffer, 1024)) > 0)
 	{
 		if (filecheck == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
 		filecheck = write(fd2, buffer, filecheck);
 		if (filecheck == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
 	if (filecheck == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (close(fd) == -1)
-		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
 	if (close(fd2) == -1)
-		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd2), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
 
 	return (0);
 }
