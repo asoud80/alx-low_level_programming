29566c431c833405ba700f45b85365f867f29dc1
asoud80 <amgad_a.soud@outlook.com>
2023-05-11 07:25:23 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fbopen, bdWrite, length = 0;
+	int fbopen, fbWrite, length = 0;
 
 	if (filename == NULL)
 		return (-1);
