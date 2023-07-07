472a721003086bc618da9e03727a40f72a4ac61a
asoud80 <amgad_a.soud@outlook.com>
2023-07-06 17:13:47 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,5 @@
-#define MAIN_H
 #ifndef MAIN_H
+#define MAIN_H
 
 void print_binary(unsigned long int n);
 int set_bit(unsigned long int *n, unsigned int index);
@@ -8,4 +8,5 @@ unsigned int binary_to_uint(const char *b);
 int get_bit(unsigned long int n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 int get_endianness(void);
+
 #endif
