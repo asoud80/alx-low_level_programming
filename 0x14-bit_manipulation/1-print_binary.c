25617a8e54ca9c46ae43c13ec9f4cf040f73c7e9
asoud80 <amgad_a.soud@outlook.com>
2023-07-06 17:22:43 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - Binary Representation Of Num will be printed
+ *
+ * @n: print a Decimal Numb As a Binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int tmp;
+	int shft;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+
+	for (tmp = n, shft = 0; (tmp >>= 1) > 0; shft++)
+		;
+
+	for (; shft >= 0; shft --)
+	{
+		if ((n >> shft) & 1)
+			printf("1");
+		else
+			printf("0");
+	}
+}
