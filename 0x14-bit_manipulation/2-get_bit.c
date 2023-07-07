ff7ab8cce3bf503926663eb8a64c75c897c8cb12
asoud80 <amgad_a.soud@outlook.com>
2023-07-06 17:47:32 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_bit – to Get  Value Of Bit in Given Indx
+ *
+ * @n: Num To be Evaluated
+ *
+ * @index: Start the Index From 0, Of a Bit Should b Got
+ *
+ * Return: the value Of a Bit of an Indx,  incase of Error -1.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int hld;
+
+	if (Indx > 64)
+		return (-1);
+
+	hld = n >> Indx;
+
+	return (hld & 1);
+}
